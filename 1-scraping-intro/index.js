194dadbec895515ef1b3b7068bb8b8cf18343740
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-17 11:06:05 -0500

    Select multiple elements

--- 1-scraping-intro/index.js
+++ 1-scraping-intro/index.js
@@ -3,13 +3,14 @@ const fs = require('fs')
 const cheerio = require('cheerio')
 
 async function main() {
-  const html = await request.get('https://reactnativetutorial.net/css-selectors')
+  const html = await request.get('https://reactnativetutorial.net/css-selectors/lesson2.html')
   fs.writeFileSync('./test.html', html)
   // now we have the page html in a file
   // time to extract from it
   const $ = await cheerio.load(html)
-  const theText = $('h1').text()
-  console.log(theText)
+  $('h2').each((index, element) => {
+    console.log($(element).text())
+  })
 }
 
 main()
