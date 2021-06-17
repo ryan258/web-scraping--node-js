279aeebf9bf9476aed742a70e56500f69ca0eb93
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-17 11:17:33 -0500

    Select by attribute

--- 1-scraping-intro/index.js
+++ 1-scraping-intro/index.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const cheerio = require('cheerio')
 
 async function main() {
-  const html = await request.get('https://reactnativetutorial.net/css-selectors/lesson2.html')
+  const html = await request.get('https://reactnativetutorial.net/css-selectors/lesson6.html')
   fs.writeFileSync('./test.html', html)
   // now we have the page html in a file
   // time to extract from it
   const $ = await cheerio.load(html)
-  $('h2').each((index, element) => {
-    console.log($(element).text())
-  })
+  console.log($('[data-customer="22293"]').text())
+  // or you can just look for the attribute
+  console.log($('[data-customer]').text())
 }
 
 main()
